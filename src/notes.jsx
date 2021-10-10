e61e2a74f6942422dbd09a6457f463fbcb5a21c1
Rahul Singh <6397rahulsingh@gmail.com>
2021-10-10 08:56:26 -0700

    finished

---
+++ src/notes.jsx
@@ -0,0 +1,11 @@
+const notes = [
+  {key:1,
+    title:"Arrays",
+    content:"In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form "
+  },
+  {key:2,
+    title:"Arrays",
+    content:"In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form "
+  }
+]
+export default notes;
